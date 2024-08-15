b11169072ca86bd94806e71ab7737b359727f3b1
LavanyaPanja <79245407+LavanyaPanja@users.noreply.github.com>
2024-08-15 21:43:23 +0530

    Create SecuirityConfig.java

---
+++ secuirity/SecuirityConfig.java
@@ -0,0 +1,67 @@
+package com.society.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.society.filter.JwtRequestFilter;
+import com.society.service.UserService;
+
+
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig  {
+
+    private final JwtRequestFilter jwtRequestFilter;
+    private final UserService userService;
+
+    public SecurityConfig(JwtRequestFilter jwtRequestFilter, UserService userService) {
+        this.jwtRequestFilter = jwtRequestFilter;
+        this.userService = userService;
+    }
+
+
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService).passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+            .authorizeRequests()
+                .requestMatchers("/users/register", "/users/login").permitAll()  // Public endpoints
+                .requestMatchers("/admin/**").hasRole("admin") // Admin-only endpoints
+                .requestMatchers("/users/**").hasAnyRole("user", "admin") // User and Admin endpoints
+                .anyRequest().authenticated()
+            .and()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+}
+
+  
