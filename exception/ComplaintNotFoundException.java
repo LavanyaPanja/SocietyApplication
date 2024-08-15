9f33dadeb37896f2943dd050a6be1ea7320b90eb
LavanyaPanja <79245407+LavanyaPanja@users.noreply.github.com>
2024-08-15 21:33:52 +0530

    Create ComplaintNotFoundException.java

---
+++ exception/ComplaintNotFoundException.java
@@ -0,0 +1,9 @@
+package com.society.exception;
+
+public class ComplaintNotFoundException extends RuntimeException {
+    public ComplaintNotFoundException(String message) {
+        super(message);
+    }
+}
+
+
